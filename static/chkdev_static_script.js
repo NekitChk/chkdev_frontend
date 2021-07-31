063d491e2620e5c3fbeb59e2007187a20a84a693
Некит <admin@chkdev.ru>
2021-07-31 23:57:51 +1000

    add source map

--- static/chkdev_static_script.js
+++ static/chkdev_static_script.js
@@ -1,5 +1,6 @@
 "use strict";
 //CHKDEV STATIC - 31.07.2021
+//# sourceMappingURL=/js/map/chkdev_static_script.js.map
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
