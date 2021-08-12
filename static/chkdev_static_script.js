12151c68a7506aa95e697a252d8d1a561a258a84
Некит <admin@chkdev.ru>
2021-08-13 03:43:27 +1000

    Update chkdev_static_script.js

--- static/chkdev_static_script.js
+++ static/chkdev_static_script.js
@@ -1,5 +1,6 @@
 "use strict";
 //CHKDEV STATIC - 13.08.2021 03:39
+//# sourceMappingURL=/js/map/chkdev_static_script.js.map
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
